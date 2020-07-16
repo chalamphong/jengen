d9752323602032473ba11ec12bb1340e66b16edd
Chalamphong Pandey <2201pandey@gmail.com>
2020-07-16 22:29:46 +0700

    Add validation to hydrate

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,16 @@
-const hydrate = (template, variables) => {
+const hydrate = (template, context) => {
+  if (typeof template !== "string") {
+    throw new Error("Cannot hydrate because template is not a string");
+  }
+
+  if (!context || typeof context !== "object" || Array.isArray(context)) {
+    throw new Error("Cannot hydrate because context is not an object");
+  }
+
   let mutableTemplate = template;
-  Object.keys(variables).forEach(variable => {
-    const toReplace = `__${variable}__`;
-    mutableTemplate = template.split(toReplace).join(variables[variable]);
+  Object.keys(context).forEach(key => {
+    const toReplace = `__${key}__`;
+    mutableTemplate = mutableTemplate.split(toReplace).join(context[key]);
   });
 
   return mutableTemplate;
